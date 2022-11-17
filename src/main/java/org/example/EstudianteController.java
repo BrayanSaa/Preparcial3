3f6e84f624d0f8bb7c44ba04b7ca0783e449e9e5
BrayanSaa <brayana.saam@uqvirtual.edu.co>
2022-11-17 17:32:35 -0500

    Primera Parte para mapear

---
+++ src/main/java/org/example/EstudianteController.java
@@ -0,0 +1,24 @@
+package org.example;
+
+
+import javafx.application.Application;
+import javafx.fxml.Initializable;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class EstudianteController implements Initializable {
+
+    public void start(Stage stage) throws Exception {
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println("Inicializando");
+
+    }
+
+
+}
